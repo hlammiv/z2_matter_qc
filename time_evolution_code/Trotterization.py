1f9edc21125123167b2422a42ad47e6d373292bd
latticeDimReg <52664943+latticeDimReg@users.noreply.github.com>
2022-03-22 11:47:46 -0500

    Monday Meeting Updates on Trotter Code
    Built a single function wrapper that will handle the various types of simulations we would want to do. 2-site and 4-site for noisy hardware and arbitrary volume for noiseless simulations. Included flags to now allow richardson extrapolations and pauli twirling

--- time_evolution_code/Trotterization.py
+++ time_evolution_code/Trotterization.py
@@ -9,12 +9,140 @@ Mike Wagman, Norman, Clement, Sarah, Elizabeth
 import Z2gates
 import numpy as np
 from scipy.linalg import expm
-from qiskit import QuantumCircuit
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
 from qiskit.quantum_info import Operator as Operator
 
 
-def trotter_evolution_2site(quantum_circuit, epsilon: float, mass: float,
-                            ntrotter: int, twirl=False):
+def trotter_evolution(num_sites: int, epsilon: float, mass: float,
+                      ntrotter: int, twirl=False, qsim=True,
+                      richardson_level: int=1, state_prep=True):
+    """
+    Wrapper function for the various types of noiseless, noisy, and harware
+    simulations that we want to do for the Z2 gauge with staggered matter
+    in (1+1)D. These simulations allow for including and excluding randomized
+    compiling (pauli twirling) and richardson extrapolations.
+
+    Parameters
+    ----------
+    num_sites : int
+        number of sites for the simulation
+    epsilon : float
+        the trotter step size
+    mass : float
+        mass of the fermion
+    ntrotter : int
+        the number of trotter steps for the simulation
+    twirl : boolean, optional
+        whether to implement this circuit with randomized
+        compiling. The default is False.
+    qsim : boolean, optional
+        Whether to set this simulation up for running on a quantum computer
+        or on a qasm/statevector simulator. The default is True
+    richardson_level : int
+        Determines how many times the CNOT gate is interleaved.
+        # N_CNOTS = richardson_level * 2 - 1
+    state_prep : boolean, (optional)
+        whether to use a state prep circuit or not. Default is True
+
+    Returns
+    -------
+    quantum_circuit : qiskit.QuantumCircuit
+        the quantum circuit that will be simulated
+
+    """
+    # the number of qubits is given by 2 * num_sites - 1
+    # because there are num_sites and num_sites - 1 gauge links
+    number_of_qubits = 2 * num_sites - 1
+    # define the quantum circuit
+    quantum_circuit = QuantumCircuit(number_of_qubits, number_of_qubits)
+    # State preparation whether or not to do it
+    if state_prep:
+        quantum_circuit.h([2 * i + 1 for i in range(num_sites - 1)])
+
+    if qsim:
+        if num_sites == 2:
+            if state_prep:
+                quantum_circuit.x([0])
+                quantum_circuit.z([1])
+            quantum_circuit = trotter_evolution_2sites(quantum_circuit,
+                                                      epsilon, mass, ntrotter,
+                                                      twirl=twirl,
+                                                      richardson_level=richardson_level)
+        elif num_sites == 4:
+            if state_prep:
+                quantum_circuit.x([2, 4])
+                quantum_circuit.z([3])
+            quantum_circuit = trotter_evolution_4sites(quantum_circuit,
+                                                      epsilon, mass, ntrotter,
+                                                      twirl=twirl,
+                                                      richardson_level=richardson_level)
+        else:
+            exception_str = "If intending to simulation on a hardware, "
+            exception_str += "only 2 and 4 site simulations are allowed."
+            raise Exception(exception_str)
+    # this code executes for the noiseless or qasm simulators
+    else:
+        if state_prep:
+            mid = 2 * (num_sites // 2)
+            quantum_circuit.x([mid, mid + 2])
+            quantum_circuit.z(mid + 1)
+        quantum_circuit = trotter_evolution_generic(quantum_circuit,
+                                                    num_sites, epsilon,
+                                                    mass, ntrotter,
+                                                    twirl=False,
+                                                    save_state_vector=True)
+
+    return quantum_circuit
+
+
+def trotter_evolution_generic(quantum_circuit, num_sites: int, epsilon: float,
+                              mass: float, ntrotter: int, twirl=False,
+                              save_state_vector=True):
+    """
+    This code implements a quantum circuit for a second order trotter using
+    the most efficient encoding of the trotterization
+
+    Parameters
+    ----------
+    quantum_circuit : qiskit.QuantumCircuit
+        quantum circuit for simulation
+    num_sites : int
+        The number of sites for the lattice
+    epsilon : float
+        the suzuki Trotter step size
+    mass : float
+        the fermion mass
+    ntrotter : int
+        the number of trotter steps
+    twirl : boolean, optional
+        whether to implement randomized compiling or not.
+        The default is False.
+    save_state_vector : boolean, optional
+        save the statevectors of the simulation. This is good if we want to
+        minimize the computing time. The default is True.
+
+    Returns
+    -------
+    quantum_circuit : qiskit.QuantumCircuit
+        quantum circuit to that we want to run for a large lattice
+
+    """
+    quantum_circuit = Z2gates.apply_mass_terms(quantum_circuit, num_sites,
+                                               mass, epsilon)
+    quantum_circuit = Z2gates.apply_gauge_terms(quantum_circuit, num_sites,
+                                                epsilon)
+    quantum_circuit = Z2gates.apply_fermion_hopping(quantum_circuit,
+                                                    num_sites, epsilon,
+                                                    eta=1.0, twirl=False)
+    quantum_circuit = Z2gates.apply_mass_terms(quantum_circuit, num_sites,
+                                               mass, epsilon)
+    quantum_circuit = Z2gates.apply_gauge_terms(quantum_circuit, num_sites,
+                                                epsilon)
+
+    return quantum_circuit
+
+def trotter_evolution_2sites(quantum_circuit, epsilon: float, mass: float,
+                            ntrotter: int, twirl=False, richardson_level=1):
     """
     apply the Suzuki-Trotter evolution operator ntrotter times on the quantum
     circuit:
@@ -37,27 +165,31 @@ def trotter_evolution_2site(quantum_circuit, epsilon: float, mass: float,
 
     Returns
     -------
-    quantum_circuit : TYPE
+    quantum_circuit : qiskit.QuantumCircuit
         quantum circuit object after the gates have been applied
 
     """
     # apply the rotations corresponding to the mass operators
-    quantum_circuit = Z2gates.apply_mass_terms(quantum_circuit, 2, mass, epsilon)
+    quantum_circuit = Z2gates.apply_mass_terms(quantum_circuit, 2, mass,
+                                               epsilon)
     # apply the rotations corresponding to the gauge dynamics operators
     quantum_circuit = Z2gates.apply_gauge_terms(quantum_circuit, 2, epsilon)
     # apply the fermion hopping term across the qubits
     quantum_circuit = Z2gates.apply_fermion_hopping_2sites(quantum_circuit,
                                                            epsilon, eta=1.0,
-                                                           twirl=twirl)
+                                                           twirl=twirl,
+                                                           richardson_level=richardson_level)
     # apply the rotations corresponding to the mass operators
-    quantum_circuit = Z2gates.apply_mass_terms(quantum_circuit, 2, mass, epsilon)
+    quantum_circuit = Z2gates.apply_mass_terms(quantum_circuit, 2, mass,
+                                               epsilon)
     # apply the rotations corresponding to the gauge dynamics operators
     quantum_circuit = Z2gates.apply_gauge_terms(quantum_circuit, 2, epsilon)
 
     return quantum_circuit
 
 def trotter_evolution_4sites(quantum_circuit, epsilon: float, mass: float,
-                             ntrotter: int, twirl=False):
+                             ntrotter: int, twirl=False,
+                             richardson_level=1):
     """
     apply the Suzuki-Trotter evolution operator ntrotter times on the quantum
     circuit:
@@ -85,15 +217,18 @@ def trotter_evolution_4sites(quantum_circuit, epsilon: float, mass: float,
 
     """
     # apply the rotations corresponding to the mass operators
-    quantum_circuit = Z2gates.apply_mass_terms(quantum_circuit, 4, mass, epsilon)
+    quantum_circuit = Z2gates.apply_mass_terms(quantum_circuit, 4, mass,
+                                               epsilon)
     # apply the rotations corresponding to the gauge dynamics operators
     quantum_circuit = Z2gates.apply_gauge_terms(quantum_circuit, 4, epsilon)
     # apply the fermion hopping term across the qubits
     quantum_circuit = Z2gates.apply_fermion_hopping_4sites(quantum_circuit,
                                                             epsilon, eta=1.0,
-                                                            twirl=twirl)
+                                                            twirl=twirl,
+                                                            richardson_level=richardson_level)
     # apply the rotations corresponding to the mass operators
-    quantum_circuit = Z2gates.apply_mass_terms(quantum_circuit, 4, mass, epsilon)
+    quantum_circuit = Z2gates.apply_mass_terms(quantum_circuit, 4, mass,
+                                               epsilon)
     # apply the rotations corresponding to the gauge dynamics operators
     quantum_circuit = Z2gates.apply_gauge_terms(quantum_circuit, 4, epsilon)
 
@@ -128,7 +263,8 @@ if __name__ == "__main__":
             r1q = rmasses[j] @ r1q
     fhops = [np.kron(fhop, np.identity(2 ** 4)),
              np.kron(np.identity(16), fhop),
-             np.kron(np.identity(4), np.kron(fhop, np.identity(4)))]
+             np.kron(np.identity(4),
+                     np.kron(fhop.conjugate().transpose(), np.identity(4)))]
 
     oper = np.identity(2 ** 7).dot(r1q)
     for op in fhops:
@@ -138,3 +274,15 @@ if __name__ == "__main__":
     opert = Operator(trotter_evolution_4sites(QuantumCircuit(7), dt, mass, 1))
     # print(np.array(oper)[:10, :10], np.array(opert)[:10, :10])
     print(oper.equiv(opert))
+    print('=' * 100)
+    try:
+        trotter_evolution(8, 0.1, 1.0, 1, twirl=False, qsim=True,
+                          state_prep=False)
+    except (Exception) as e:
+        print('====' * 25)
+        print(e)
+        print('---')
+        print('exception working')
+    print('=' * 100)
+    trotter_evolution(8, 0.1, 1.0, 1, twirl=False, qsim=False,
+                      state_prep=False)
--- time_evolution_code/Z2gates.py
+++ time_evolution_code/Z2gates.py
@@ -77,8 +77,74 @@ def apply_gauge_terms(quantum_circuit, number_of_sites, epsilon):
     return quantum_circuit
 
 
+def apply_fermion_hopping(quantum_circuit, number_of_sites : int,
+                          epsilon : float,
+                          eta=1.0, twirl=False):
+    '''
+    apply a fermion hoppping term across an arbitrary connectivity grid and
+    number of lattice sites.
+
+    Parameters
+    ----------
+    quantum_circuit : qiskit.QuantumCircuit
+        quantum circuit to add the fermion hopping term to
+    number_of_sites : int
+        number of physical lattice sites
+    epsilon : float
+        the Suzuki-Trotter step size
+    eta : float, optional
+        anisotropy (maybe needed to renormalize the speed of light).
+        The default is 1.0.
+    twirl : boolean, optional
+        whether to twirl or not. NOT CURRENTLY IMPLEMENTED.
+        The default is False.
+
+    Returns
+    -------
+    quantum_circuit : qiskit.QuantumCircuit
+        the quantum circuit with the fermion hopping gates appended
+
+    '''
+    # iterate through the even sites
+    for site in range(0, number_of_sites - 1, 2):
+        q1, q2, q3 = 2 * site, 2 * site + 1, 2 * site + 2
+        quantum_circuit.z(q1)
+        quantum_circuit.sxdg(q2)
+        quantum_circuit.s(q2)
+        quantum_circuit.cx(q1, q2)
+        quantum_circuit.sx(q1)
+        quantum_circuit.cx(q1, q3)
+        quantum_circuit.rx(epsilon * eta / 4, q1)
+        quantum_circuit.ry(epsilon * eta / 4, q3)
+        quantum_circuit.cx(q1, q3)
+        quantum_circuit.sxdg(q1)
+        quantum_circuit.cx(q1, q2)
+        quantum_circuit.sdg(q2)
+        quantum_circuit.sx(q2)
+        quantum_circuit.z(q1)
+    # iterate through the odd sites
+    for site in range(1, number_of_sites - 1, 2):
+        q1, q2, q3 = 2 * site, 2 * site + 1, 2 * site + 2
+        quantum_circuit.z(q1)
+        quantum_circuit.sxdg(q2)
+        quantum_circuit.s(q2)
+        quantum_circuit.cx(q1, q2)
+        quantum_circuit.sx(q1)
+        quantum_circuit.cx(q1, q3)
+        quantum_circuit.rx(-epsilon * eta / 4, q1)
+        quantum_circuit.ry(-epsilon * eta / 4, q3)
+        quantum_circuit.cx(q1, q3)
+        quantum_circuit.sxdg(q1)
+        quantum_circuit.cx(q1, q2)
+        quantum_circuit.sdg(q2)
+        quantum_circuit.sx(q2)
+        quantum_circuit.z(q1)
+    return quantum_circuit
+
+
 def apply_fermion_hopping_2sites(quantum_circuit, epsilon,
-                                  eta=1.0, twirl=False):
+                                  eta=1.0, twirl=False,
+                                  richardson_level=1):
     """
 
     apply the 4 cnot version of the fermion hopping gate shown in the overleaf
@@ -94,6 +160,8 @@ def apply_fermion_hopping_2sites(quantum_circuit, epsilon,
         an scale for renormalization of speed of light (?)
     twirl : boolean (optional)
         whether to use twirled or not twirled gates
+    richardson_level: int (optional)
+        the richardson scaling level
 
     Returns
     -------
@@ -102,46 +170,61 @@ def apply_fermion_hopping_2sites(quantum_circuit, epsilon,
 
     """
 
+    # define the number of times to apply a cnot
+    ncnots = richardson_level * 2 - 1
     # apply the unitary to semi-diagonalize the fermion hopping gate
     quantum_circuit.z(0)
     quantum_circuit.sxdg(1)
     quantum_circuit.s(1)
     # if the twirl flag is use applied a cnot twirl
     if twirl:
-        quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
-                                                            3, (0, 1))
+        for n in range(ncnots):
+            quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
+                                                                3, (0, 1))
     # otherwise just apply a regular cnot
     else:
-        quantum_circuit.cx(0, 1)
+        for n in range(ncnots):
+            quantum_circuit.barrier(0, 1)
+            quantum_circuit.cx(0, 1)
     # apply the next single qubit gate
     quantum_circuit.sx(0)
     # if the twirl flag is used apply a cnot twirl
     if twirl:
-        quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
+        for n in range(ncnots):
+            quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
                                                             3, (0, 2))
     # otherwise just apply a regular cnot
     else:
-        quantum_circuit.cx(0, 2)
+        for n in range(ncnots):
+            quantum_circuit.barrier(0, 2)
+            quantum_circuit.cx(0, 2)
     # apply the single qubit phase rotations
     quantum_circuit.rx(epsilon * eta / 4, 0)
     quantum_circuit.ry(epsilon * eta / 4, 2)
     # now we begin the process of inverting the semi-diagonalizing operator
     # twirl the cnot if flag is raised
     if twirl:
-        quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
-                                                            3, (0, 2))
+
+        for n in range(ncnots):
+            quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
+                                                                3, (0, 2))
     # otherwise do nothing
     else:
-        quantum_circuit.cx(0, 2)
+        for n in range(ncnots):
+            quantum_circuit.barrier(0, 2)
+            quantum_circuit.cx(0, 2)
     # single qubit gate
     quantum_circuit.sxdg(0)
     # twirl the CNOT from p to gamma if flag raised
     if twirl:
-        quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
+        for n in range(ncnots):
+            quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
                                                             3, (0, 1))
     # otherwise do nothing
     else:
-        quantum_circuit.cx(0, 1)
+        for n in range(ncnots):
+            quantum_circuit.barrier(0, 1)
+            quantum_circuit.cx(0, 1)
     # final single qubit gates
     quantum_circuit.sdg(1)
     quantum_circuit.sx(1)
@@ -150,7 +233,8 @@ def apply_fermion_hopping_2sites(quantum_circuit, epsilon,
     return quantum_circuit
 
 
-def apply_fermion_hopping_4sites(quantum_circuit, epsilon, eta=1.0, twirl=False):
+def apply_fermion_hopping_4sites(quantum_circuit, epsilon, eta=1.0,
+                                 twirl=False, richardson_level=1):
     """
       perform the fermion hopping operation across a 4 site staggered
       fermion lattice
@@ -167,14 +251,17 @@ def apply_fermion_hopping_4sites(quantum_circuit, epsilon, eta=1.0, twirl=False)
     twirl : boolean, optional
         whether to pauli twirl (randomized compiling)
         or not. The default is False.
-
+    richardson_level : int, optional
+        how much to distort the cnot gates
     Returns
     -------
     quantum_circuit : qiskit.QuantumCircuit
         the quantum_circuit object with the gates applied.
 
     """
-
+    # this the relation to distort the cnot gates by for a given richardson
+    # level
+    ncnots = richardson_level * 2 - 1
     # single qubit rotations for the hopping from sites 0-1 and 2-3
     #================#
     # fermion term 1 #
@@ -186,25 +273,37 @@ def apply_fermion_hopping_4sites(quantum_circuit, epsilon, eta=1.0, twirl=False)
     # (simultaneously applied )
     #=========================
     if twirl:
-        quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
-                                                            7, [(0, 1),
-                                                                (4, 5)])
+        # iterate the twirled cnots
+        for n in range(ncnots):
+            quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
+                                                                7, [(0, 1),
+                                                                    (4, 5)])
     # otherwise just a regular cnot across (2, 1) and (4, 5)
     else:
-        quantum_circuit.cx(0, 1)
-        quantum_circuit.cx(4, 5)
+        # iterate the untwirled cnots
+        for n in range(ncnots):
+            quantum_circuit.barrier(0, 1)
+            quantum_circuit.barrier(4, 5)
+            quantum_circuit.cx(0, 1)
+            quantum_circuit.cx(4, 5)
     # single qubit gate rotation
     #================================
     quantum_circuit.sx([0, 4])
     # if twirling flag is raised apply the second CNOT gate
     if twirl:
-        quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
-                                                            7, [(0, 2),
-                                                                (4, 6)])
+        # iterate the twirled cnots
+        for n in range(ncnots):
+            quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
+                                                                7, [(0, 2),
+                                                                    (4, 6)])
     # otherwise just apply a plain old cnot across 2-0 and 4-6
     else:
-        quantum_circuit.cx(0, 2)
-        quantum_circuit.cx(4, 6)
+        # iterate the untwirled cnots
+        for n in range(ncnots):
+            quantum_circuit.barrier(0, 2)
+            quantum_circuit.barrier(4, 6)
+            quantum_circuit.cx(0, 2)
+            quantum_circuit.cx(4, 6)
     #single qubit rotions for the evolution
     #===============================
     quantum_circuit.rx(epsilon / 2, [0, 4])
@@ -213,25 +312,37 @@ def apply_fermion_hopping_4sites(quantum_circuit, epsilon, eta=1.0, twirl=False)
 
     # if twirling flag is raised apply the second CNOT gate
     if twirl:
-        quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
-                                                            7, [(0, 2),
-                                                                (4, 6)])
+        # iterate the twirled cnots
+        for n in range(ncnots):
+            quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
+                                                                7, [(0, 2),
+                                                                    (4, 6)])
     # otherwise just apply a plain old cnot across 2-0 and 4-6
     else:
-        quantum_circuit.cx(0, 2)
-        quantum_circuit.cx(4, 6)
+        # iterate the untwirled cnots
+        for n in range(ncnots):
+            quantum_circuit.barrier(0, 2)
+            quantum_circuit.barrier(4, 6)
+            quantum_circuit.cx(0, 2)
+            quantum_circuit.cx(4, 6)
     #===============================
     quantum_circuit.sxdg([0, 4])
     #=========================
     #=========================
     if twirl:
-        quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
-                                                            7, [(0, 1),
-                                                                (4, 5)])
+        # iterate the twirled cnots
+        for n in range(ncnots):
+            quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
+                                                                7, [(0, 1),
+                                                                    (4, 5)])
     # otherwise just a regular cnot across (2, 1) and (4, 5)
     else:
-        quantum_circuit.cx(0, 1)
-        quantum_circuit.cx(4, 5)
+        # iterate the cnots
+        for n in range(ncnots):
+            quantum_circuit.barrier(0, 1)
+            quantum_circuit.barrier(4, 5)
+            quantum_circuit.cx(0, 1)
+            quantum_circuit.cx(4, 5)
     #================================
     quantum_circuit.sdg([1, 5])
     quantum_circuit.sx([1, 5])
@@ -244,24 +355,36 @@ def apply_fermion_hopping_4sites(quantum_circuit, epsilon, eta=1.0, twirl=False)
     # if the twirling flag is raised apply a cnot hard cycle twirl here
     # otherwise just leave it as is
     if twirl:
-        quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
-                                                            7, (3, 2))
-        quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
-                                                            7, (3, 4))
+
+        for n in range(ncnots):
+            quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
+                                                                7, (3, 2))
+
+        for n in range(ncnots):
+            quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
+                                                                7, (3, 4))
     else:
-        quantum_circuit.cx(3, 2)
-        quantum_circuit.cx(3, 4)
+        for n in range(ncnots):
+            quantum_circuit.barrier(3, 2)
+            quantum_circuit.cx(3, 2)
+        for n in range(ncnots):
+            quantum_circuit.barrier(3, 4)
+            quantum_circuit.cx(3, 4)
     # some more single qubit rotations
     #================================
-    quantum_circuit.rx(epsilon / 2, 3)
+    quantum_circuit.rx(-epsilon / 2, 3)
     quantum_circuit.h(2)
     # if the twirling flag is raised apply a cnot hard cycle twirl here
     # otherwise just leave it as is
     if twirl:
-        quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
-                                                            7, (3, 2))
+
+        for n in range(ncnots):
+            quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
+                                                                7, (3, 2))
     else:
-        quantum_circuit.cx(3, 2)
+        for n in range(ncnots):
+            quantum_circuit.barrier(3, 2)
+            quantum_circuit.cx(3, 2)
     #================================
     quantum_circuit.h(2)
     quantum_circuit.s(2)
@@ -271,13 +394,16 @@ def apply_fermion_hopping_4sites(quantum_circuit, epsilon, eta=1.0, twirl=False)
     # if the twirling flag is raised apply a cnot hard cycle twirl here
     # otherwise just leave it as is
     if twirl:
-        quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
-                                                            7, (3, 4))
+        for n in range(ncnots):
+            quantum_circuit = circuit_twirling.twirl_hard_cycle(quantum_circuit,
+                                                                7, (3, 4))
     else:
-        quantum_circuit.cx(3, 4)
+        for n in range(ncnots):
+            quantum_circuit.barrier(3, 4)
+            quantum_circuit.cx(3, 4)
     quantum_circuit.h(4)
     quantum_circuit.s(4)
-    quantum_circuit.rx(epsilon / 2, 3)
+    quantum_circuit.rx(-epsilon / 2, 3)
     # if the twirling flag is raised apply a cnot hard cycle twirl here
     # otherwise just leave it as is
     if twirl:
