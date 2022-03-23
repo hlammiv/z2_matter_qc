7a20cfe68d21d7113dfabc38f512fefa8a6b38a9
Norman Hogan <ahogan9@uic.edu>
2022-03-23 10:20:14 -0400

    Update extrapolations.py
    Added more code for the exponential fitting

--- pauli_twirling/extrapolations.py
+++ pauli_twirling/extrapolations.py
@@ -13,7 +13,6 @@ from qiskit.tools.monitor import job_monitor
 from qiskit.quantum_info import Operator 
 import qiskit.quantum_info as qi
 
-import numpy
 import qiskit"""
 
 
@@ -32,13 +31,17 @@ data  =    {    [key]   :  [ step1[<O>_q0,<O>_q1,<O>_q2],  step2[<O>_q0,<O>_q1,<
  data[3] gives all data with 3:1 cnots in our expected circuit
 
  data[1][0] gives the observable of each qubit at the first trotter step and
- data[1][2][1] gives the observable measured on qubit 1 for the third trottter step
+ data[1][2][1] gives the observable measured on qubit 1 for the third trotter step
+
+ data[5][2][1] also gives the observable measured on qubit 1 for the third trotter step,
+               but the cnots have a multiplicity of 5
 
 
 """
 
 
 import numpy
+import gvar as gv
 
 def noiserescale(target_observable,estimation_observable):
     """
@@ -65,7 +68,7 @@ def noiserescale(target_observable,estimation_observable):
     return rescaled_observable
 
 
-def ZNEfits(observable, NCX):
+def ZNEfits(observable, NCX, nqubits, nsteps):
     """
     Uses exponential fits to determine the zero-noise limit
 
@@ -76,13 +79,33 @@ def ZNEfits(observable, NCX):
         For each multiplicity of cnots (keys), the observable at each trotter step (values)
     NCX : list
         The multiplicities of cnots for each step of the zero-noise extrapolation (ZNE)
+    nqubits: int
+        The total number of qubits
+    nsteps: int
+        The total number of trotter steps
     
     Returns
     -------
-    fits : list of lsqfit.nonlinear_fit
-        The fits and their parameters for each multiplicity of cnots
+    fits : Dictionary of lsqfit.nonlinear_fit
+        The fits and their parameters for each qubit(key)
     
     """
+
+    #Create the exponential function and its prior parameters
+    exponential = lambda x, p: [p['A'] * gv.exp(-p['B'] * x[i]) for i in range(len(x))]
+    prior = {'f(A)': gv.BufferDict.uniform('f', 1, -1), 'log(B)': gv.gvar(0, 2000)}
+
+    #Initiate the dictonary of fits
+    fits = {}
+
+    #Loop over each trotter step
+    for step in range(nsteps):
+        #Loop over each qubit
+        for qubit in range(nqubits)
+            fits['q'+str(qubit)] = lsqfit.nonlinear_fit(data=(NCX,[observable[j][step][qubit] for j in NCX]), 
+                                    prior=prior, fcn=exponential, debug=True)
+
+    return fits
 
     
 
